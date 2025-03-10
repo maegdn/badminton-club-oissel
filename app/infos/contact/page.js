51370871b72e9684ab878c4d96be512367c2af57
Maelys Guardini <louguardini@gmail.com>
2025-03-10 01:09:12 +0100

    contact form work: handling value changes and submission

--- app/infos/contact/page.js
+++ app/infos/contact/page.js
@@ -13,6 +13,22 @@ export default function Contact() {
   });
   const [submitted, setSubmitted] = useState(false);
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((previousState) => ({ ...previousState, [name]: value }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Data Submitted:", formData);
+    setSubmitted(true);
+    setFormData({
+      name: "",
+      firstname: "",
+      email: "",
+      message: "",
+    });
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
@@ -23,33 +39,54 @@ export default function Contact() {
             <p>
               Pour nous contacter, vous pouvez nous envoyer un email à
               <span className="font-bold"> obadoissel@gmail.com</span> ou bien
-              remplir le formulaire de contacter ci-dessous.
+              remplir le formulaire de contact ci-dessous.
             </p>
           </div>
 
-          <form className="">
+          <form className="" onSubmit={handleSubmit}>
             <div className="flex flex-col w-full gap-5 mb-6">
               <div className="flex flex-row gap-6">
                 <input
+                  type="text"
+                  name="name"
+                  value={formData.name}
                   placeholder="Nom"
                   className="bg-blue-100 rounded-md h-10 pl-3 w-2/4"
+                  onChange={handleChange}
                 ></input>
                 <input
+                  type="text"
+                  name="firstname"
+                  value={formData.firstname}
                   placeholder="Prénom"
+                  onChange={handleChange}
                   className="bg-blue-100 rounded-md h-10 pl-3 w-2/4"
                 ></input>
               </div>
               <input
+                type="text"
+                required
+                name="email"
+                value={formData.email}
                 placeholder="Adresse e-mail"
                 className="bg-blue-100 rounded-md h-10 pl-3"
+                onChange={handleChange}
               ></input>
               <input
+                type="text"
+                value={formData.message}
+                required
+                name="message"
                 placeholder="Veuillez développer votre demande"
                 className="bg-gray-200 rounded-md h-20 pl-3"
+                onChange={handleChange}
               ></input>
             </div>
             <div className="flex justify-center items-center">
-              <button className="bg-blue-200 rounded-md h-12 p-3 font-bold">
+              <button
+                type="submit"
+                className="bg-blue-200 rounded-md h-12 p-3 font-bold"
+              >
                 Envoyer
               </button>
             </div>
