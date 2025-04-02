c268b1518033f7dfbf9de62decce88dffb545d63
Maelys Guardini <louguardini@gmail.com>
2025-04-02 19:48:20 +0200

    SEO component creation

---
+++ app/components/SEO.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function SEO({ description, title, url }) {
+  const siteTitle = "Oissel Badminton Club";
+  const fullTitle = title ? `${title} | ${siteTitle}` : siteTitle;
+
+  return (
+    <Head>
+      <title>{fullTitle}</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={fullTitle} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <link rel="canonical" href={url} />
+    </Head>
+  );
+}
