20cc4be1604a02ded919d99109d6dcb6813af975
Mae <maelys.guardini@gmail.com>
2025-10-30 13:58:02 +0100

    Updating web dev info mentions légales

--- app/infos/legal/page.js
+++ app/infos/legal/page.js
@@ -7,33 +7,53 @@ export default function Legal() {
     <>
       <Header3 />
       <SEO
-  title="Mentions légales - Badminton Club de Oissel"
-  url="https://badminton-club-oissel.vercel.app/infos/mentions-legales"
-  description="Mentions légales du site officiel du club de badminton de Oissel."
-  noIndex={true}
-/>
-
+        title="Mentions légales - Badminton Club de Oissel"
+        url="https://badminton-club-oissel.vercel.app/infos/mentions-legales"
+        description="Mentions légales du site officiel du club de badminton de Oissel."
+        noIndex={true}
+      />
 
       <main className="flex flex-col w-full min-h-screen grow items-center">
         <div className="flex flex-col justify-start text-justify lg:w-3/4 px-10 py-10">
           <h1 className="flex justify-center">Mentions légales</h1>
 
-          <section className="mt-12 mb-6 justify-start text-start" aria-label="Conception du site">
+          <section
+            className="mt-12 mb-6 justify-start text-start"
+            aria-label="Conception du site"
+          >
             <h2 className="text-xl font-semibold">Éditeur du site</h2>
             <p>Nom de l'éditeur : O'Bad - Badminton Oissel Club</p>
             <p>Adresse : 1441 Av. du Général de Gaulle, 76350 OISSEL</p>
-            <p>📧 Email : <a href="mailto:contact@mguardini.pro" className="text-blue-400 underline">contact@mguardini.pro</a></p>
+            <p>
+              📧 Email :{" "}
+              <a
+                href="mailto:contact@mguardini.pro"
+                className="text-blue-400 underline"
+              >
+                contact@mguardini.pro
+              </a>
+            </p>
             <p>Téléphone : 02.32.91.13.41</p>
             <p>SIRET : 499437093</p>
             <p>RNA : W763010968</p>
           </section>
 
           <section className="mb-6">
-  <h2 className="text-xl font-semibold">Hébergement</h2>
-  <p>Hébergeur : Vercel Inc.</p>
-  <p>Adresse : 340 S Lemon Ave #4133, Walnut, CA 91789, USA</p>
-  <p>Site : <a href="https://vercel.com" target="_blank" rel="noopener noreferrer" className="text-blue-400 underline">vercel.com</a></p>
-</section>
+            <h2 className="text-xl font-semibold">Hébergement</h2>
+            <p>Hébergeur : Vercel Inc.</p>
+            <p>Adresse : 340 S Lemon Ave #4133, Walnut, CA 91789, USA</p>
+            <p>
+              Site :{" "}
+              <a
+                href="https://vercel.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-400 underline"
+              >
+                vercel.com
+              </a>
+            </p>
+          </section>
 
           <section className="mb-6 justify-start text-start">
             <h2 className="text-xl font-semibold">
@@ -43,18 +63,35 @@ export default function Legal() {
               Le site a été conçu et développé par :<br />
               Maëlys Guardini - Développeuse web indépendante
             </p>
-            <p>📧 Email : contact@mguardini.pro</p>
-            <p>🗒 Portfolio : <a href="https://www.mguardini.pro" target="_blank" rel="noopener noreferrer" className="text-blue-400 underline">www.mguardini.pro</a></p>
+            <p>📧 Email : contact@mguardini.dev</p>
+            <p>
+              🗒 Portfolio :{" "}
+              <a
+                href="https://www.mguardini.dev"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-400 underline"
+              >
+                www.mguardini.dev
+              </a>
+            </p>
           </section>
 
           <section className="mb-6 justify-start">
             <h2 className="text-xl font-semibold">Propriété intellectuelle</h2>
             <p>
-  L’ensemble des contenus présents sur ce site (textes, images, logos, graphismes, etc.) sont la propriété exclusive du O'Bad – Badminton Club de Oissel ou de ses partenaires, sauf mention contraire, et sont protégés par le droit d’auteur.
-  <br /><br />
-  Certaines images ou illustrations peuvent provenir de banques d’images libres de droit (ex. Unsplash, Pexels) et sont utilisées conformément à leurs conditions d'utilisation.
-  Toute reproduction, modification ou diffusion des contenus sans autorisation écrite préalable est strictement interdite.
-</p>
+              L’ensemble des contenus présents sur ce site (textes, images,
+              logos, graphismes, etc.) sont la propriété exclusive du O'Bad –
+              Badminton Club de Oissel ou de ses partenaires, sauf mention
+              contraire, et sont protégés par le droit d’auteur.
+              <br />
+              <br />
+              Certaines images ou illustrations peuvent provenir de banques
+              d’images libres de droit (ex. Unsplash, Pexels) et sont utilisées
+              conformément à leurs conditions d'utilisation. Toute reproduction,
+              modification ou diffusion des contenus sans autorisation écrite
+              préalable est strictement interdite.
+            </p>
           </section>
 
           {/* <section className="mb-6">
@@ -95,8 +132,10 @@ export default function Legal() {
               Les présentes mentions légales sont soumises au droit français.
             </p>
 
-            <p>Ce site ne collecte aucune donnée personnelle via formulaire ou compte utilisateur.</p>
-
+            <p>
+              Ce site ne collecte aucune donnée personnelle via formulaire ou
+              compte utilisateur.
+            </p>
           </section>
         </div>
         <div>
